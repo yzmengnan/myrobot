a94975c5e241afe0ba5b5523c832f1919e2f3e81
DESKTOP-PBA5JL2\91418 <914181249@qq.com>
2023-03-16 13:01:37 +0800

    检查是否因为pp 的延迟 导致 PP lag现象

--- src/myThreadfuc.cpp
+++ src/myThreadfuc.cpp
@@ -56,6 +56,7 @@ void myThreadfuc::DRIVE(std::atomic_int &runflag, std::vector<DTS> &sdata, std::
             error_code = myservo.Servo_PTP_Basic_isSync(sdata, gdata, CIOFF, 1);
             std::cout << "DRIVE MODE:" << runflag << std::endl;
 //            std::cout << "Target_position:" << sdata[0].Target_Pos << "," << sdata[1].Target_Pos << std::endl;
+            //检查是否因为此处延时导致PP lag现象
             std::this_thread::sleep_for(std::chrono::milliseconds(200));
         } else if (runflag == 3 && servo_csp_flag) {
             std::cout << "DRIVE MODE:" << runflag << std::endl;
