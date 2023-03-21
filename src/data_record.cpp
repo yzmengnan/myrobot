7526667ef6992af72cd852216d579edfd9a5bde9
DESKTOP-PBA5JL2\91418 <914181249@qq.com>
2023-03-21 18:45:19 +0800

    更新了保存的数据,优化了结构

--- src/data_record.cpp
+++ src/data_record.cpp
@@ -2,12 +2,23 @@
 // Created by LR on 2023/3/15.
 //
 
-#include "data_record.h"
-void data_record(vector<vector<DFS>>& data){
+#include "Data_Record.h"
+void data_record(vector<vector<DFS>> &data) {
     ofstream outfile;
     outfile.open("./Data/data_record.txt", ios::out | ios::trunc);
     for (int i = 0; i < data.size(); i++) {
-        outfile << "NO." << i << " " << double(data[i][0].Actual_Vec /8388608.0*60)<< ',' << double(data[i][1].Actual_Vec /8388608.0*60)<< endl;
+        string temp;
+        for(auto child:data[i]){
+            temp+=std::to_string(child.Actual_Vec/8388608.0*60);
+            temp+=",";
+        }
+        outfile<<temp<<std::endl;
+//        outfile << "NO." << i << " " << double(data[i][0].Actual_Vec / 8388608.0 * 60) << ',' << double(data[i][1].Actual_Vec / 8388608.0 * 60)<< ','
+//                << double(data[i][2].Actual_Vec / 8388608.0 * 60) << ','<< double(data[i][3].Actual_Vec / 8388608.0 * 60)<< ','
+//                << double(data[i][4].Actual_Vec / 8388608.0 * 60)<< ',' << double(data[i][5].Actual_Vec / 8388608.0 * 60)<< ','
+//                <<double(data[i][6].Actual_Vec / 8388608.0 * 60)<< ','<<double(data[i][7].Actual_Vec / 8388608.0 * 60)<< ','
+//                <<double(data[i][8].Actual_Vec / 8388608.0 * 60)
+//                << endl;
     }
     outfile.close();
 }
