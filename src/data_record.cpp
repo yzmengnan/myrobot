00e366a383443e26d06a3a9e2a3931d194f7acec
DESKTOP-PBA5JL2\91418 <914181249@qq.com>
2023-03-27 16:06:20 +0800

    修订了数据记录

--- src/data_record.cpp
+++ src/data_record.cpp
@@ -3,16 +3,17 @@
 //
 
 #include "Data_Record.h"
+#include "Data_Process.h"
 void data_record(vector<vector<DFS>> &data) {
-    ofstream outfile;
-    outfile.open("./Data/data_record.txt", ios::out | ios::trunc);
+    ofstream outfile,outfile2,outfile3;
+    outfile.open("./Data/data_record_mul_v.txt", ios::out | ios::trunc);
     for (int i = 0; i < data.size(); i++) {
         string temp;
         for(auto child:data[i]){
             temp+=std::to_string(child.Actual_Vec/8388608.0*60);
             temp+=",";
         }
-        outfile<<"No."<<i<<temp<<std::endl;
+        outfile<<" "<<i<<","<<temp<<std::endl;
 //        outfile << "NO." << i << " " << double(data[i][0].Actual_Vec / 8388608.0 * 60) << ',' << double(data[i][1].Actual_Vec / 8388608.0 * 60)<< ','
 //                << double(data[i][2].Actual_Vec / 8388608.0 * 60) << ','<< double(data[i][3].Actual_Vec / 8388608.0 * 60)<< ','
 //                << double(data[i][4].Actual_Vec / 8388608.0 * 60)<< ',' << double(data[i][5].Actual_Vec / 8388608.0 * 60)<< ','
@@ -21,4 +22,25 @@ void data_record(vector<vector<DFS>> &data) {
 //                << endl;
     }
     outfile.close();
+    outfile2.open("./Data/data_record_mul_p.txt", ios::out | ios::trunc);
+    for(int i =0;i<data.size();i++){
+        string temp;
+        for(int j =0;j<Servo_number;j++){
+            temp+=std::to_string(dp::p2t(data[i][j].Actual_Pos,j));
+            temp+=",";
+        }
+        outfile2<<" "<<i<<","<<temp<<std::endl;
+    }
+    outfile2.close();
+
+    outfile3.open("./Data/data_record_mul_t.txt", ios::out | ios::trunc);
+    for(int i =0;i<data.size();i++){
+        string temp;
+        for(int j =0;j<Servo_number;j++){
+            temp+=std::to_string(data[i][j].Actual_Torque);
+            temp+=",";
+        }
+        outfile3<<" "<<i<<","<<temp<<std::endl;
+    }
+    outfile3.close();
 }
